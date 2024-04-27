1b093ecf27667f5d5808f6d393b9b8a6005e2e73
Tauhid hossen <tauhidhossen04@gmail.com>
2024-04-27 16:53:26 +0600

    Added card on Card Section

--- src/Components/Root/Card/Card.jsx
+++ src/Components/Root/Card/Card.jsx
@@ -1,20 +1,54 @@
 import { useEffect, useState } from "react";
 
 const Card = () => {
- 
-    const [Cards,setCards]=useState([]);
-useEffect(()=>{
-    fetch('http://localhost:5000/Place')
-    .then(res => res.json())
-    .then(data => setCards(data))
-},[]);
-console.log(Cards);
-    
-    return (
-        <div>
-            {Cards.length}
+  const [Cards, setCards] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/Place")
+      .then((res) => res.json())
+      .then((data) => setCards(data));
+  }, []);
+  console.log(Cards);
+
+  return (
+    <div className="  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4 mt-10 ">
+      {Cards.map((Card) => (
+        <div
+          data-aos="zoom-in"
+          data-aos-duration="3000"
+          key={Card._id}
+          className=" rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800 "
+        >
+          <img
+            src={Card?.image}
+            alt=""
+            className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500"
+          />
+          <div className="flex flex-col justify-between p-6 space-y-8">
+            <div className="space-y-2">
+              <h2 className="text-3xl font-semibold tracking-wide">{}</h2>
+              {/* <p className="dark:text-gray-800">{Card?.slice(0, 75)}</p> */}
+            </div>
+            <div className="flex justify-between">
+              <div>
+                <h2 className="text-xl font-semibold">Price : {}</h2>
+              </div>
+              <div>
+                <h2 className="text-xl font-semibold">Area : {}</h2>
+              </div>
+            </div>
+
+            <button
+              type="button"
+              className="   bg-slate-700 text-white  text-xl flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50"
+              // fdprocessedid="led2je"
+            >
+              Read more
+            </button>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Card;
