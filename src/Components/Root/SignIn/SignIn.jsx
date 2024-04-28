954fd15fca57bd8f0fbd6e110532058d7f61a053
Tauhid hossen <tauhidhossen04@gmail.com>
2024-04-29 02:27:22 +0600

    create a SignIn page

--- src/Components/Root/SignIn/SignIn.jsx
+++ src/Components/Root/SignIn/SignIn.jsx
@@ -1,46 +1,190 @@
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { GithubAuthProvider, GoogleAuthProvider } from "firebase/auth/cordova";
+import Swal from "sweetalert2";
+import { AuthContext } from "../../../Provider/AuthProvider";
+
 
 const SignIn = () => {
+
+    const { login, googleLogin, setUser, GitHubLogin } = useContext(AuthContext);
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location.state?.from?.pathname || "/";
+  const provider = new GoogleAuthProvider();
+  const GitHubProvider = new GithubAuthProvider();
+  
+    // login with email & pass
+  const handleLogin = (event) => {
+    event.preventDefault();
+    setError("");
+    setSuccess("");
+    // console.log(event);
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email,password);
+    login(email, password)
+      .then((result) => {
+        // eslint-disable-next-line no-unused-vars
+        const loggedUser = result.user;
+        // console.log(loggedUser);
+        navigate(from);
+        form.reset();
+        Swal.fire({
+          title: "success!",
+          text: "Login Succesfull",
+          icon: "success",
+          confirmButtonText: "Cool",
+        });
+      })
+      .catch((error) => {
+        // console.log(error.message);
+        setError(error.message);
+      });
+  };
+
+  // login with google
+  const handleGoogleLogin = () => {
+    setError("");
+    setSuccess("");
+
+    googleLogin(provider)
+      .then((result) => {
+        const user = result.user;
+        navigate(from);
+        setUser(user);
+        // console.log(user);
+        Swal.fire({
+          title: "success!",
+          text: "Login Succesfull",
+          icon: "success",
+          confirmButtonText: "OK",
+        });
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+  //github login
+  const handleGitHubLogin = () => {
+    setError("");
+    setSuccess("");
+
+    GitHubLogin(GitHubProvider)
+      .then((result) => {
+        const user = result.user;
+        navigate(from);
+        setUser(user);
+        
+        Swal.fire({
+          title: "success!",
+          text: "Login Succesfull",
+          icon: "success",
+          confirmButtonText: "OK",
+        });
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+    
     return (
-        <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-          </div>
-          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input type="email" placeholder="email" className="input input-bordered" required />
+        <div className="mt-10 mb-10">
+        {/* <Helmet>
+          <title>Indrusto || Login</title>
+        </Helmet> */}
+        <div className="hero-content">
+          <div className="card w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="p-10">
+              <h1 className="text-3xl font-bold text-center mb-2">Login</h1>
+              <form onSubmit={handleLogin}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="email"
+                    name="email"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control relative ">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type={show ? "text" : "password"}
+                    placeholder="password"
+                    name="password"
+                    className="input input-bordered"
+                    required
+                  />
+                  <p className="mt-2 absolute right-2 top-[45px]" onClick={() => setShow(!show)}>
+                  {/* <span className=" font-bold ">
+                    {show ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye> }
+                  </span> */}
+                </p>
+                </div>
+                {/* <p className="mt-2" onClick={() => setShow(!show)}>
+                  <span className=" font-bold ">
+                    {show ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye> }
+                  </span>
+                </p> */}
+                <p className="text-green-700">{success}</p>
+                <br />
+                <p className="text-red-700 ">{error}</p>
+                <div className="form-control mt-2">
+                  <input
+                    className="btn btn-outline text-orange-400"
+                    type="submit"
+                    value="Log in"
+                  />
+                </div>
+              </form>
+  
+              <p className="mt-2 mb-2">
+                {" "}
+                <span className="font-semibold">
+                  {" "}
+                  Did not have an Account?
+                </span>{" "}
+                <Link className="text-orange-400 font-bold" to="/SignUp">
+                  Register
+                </Link>
+              </p>
+              <div className="text-center">
+                <span className="text-gray-400">or</span>
               </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input type="password" placeholder="password" className="input input-bordered" required />
-                <div className="flex justify-between">
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
-                <label className="label">
-                 <Link to="/SignUp">
-                 <a href="#" className="label-text-alt link link-hover">Register Now</a>
-                 </Link>
-                </label>
+              <div className=" mx-auto mt-2">
+                <div className="  ">
+                  <button
+                    onClick={handleGoogleLogin}
+                    className=" px-6  py-3 btn btn-outline text-orange-400 rounded-md mb-4"
+                  >
+                      Sign in with Google
+                  </button>
+                  <button
+                    onClick={handleGitHubLogin}
+                    className=" px-6  py-3 btn btn-outline text-orange-400 rounded-md btn-error"
+                  >
+                    Sign in with GitHub
+                  </button>
                 </div>
               </div>
-              <div>
-                <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
-              </div>
-              </div>
-            </form>
+            </div>
           </div>
         </div>
       </div>
-    );
+      );
 };
+
 
 export default SignIn;
