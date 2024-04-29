7c8006cf1c4b71173f93d64e9b03aa73175d2479
Tauhid hossen <tauhidhossen04@gmail.com>
2024-04-29 08:38:00 +0600

    Banner section slider added

--- src/Components/Root/Banner/Banner.jsx
+++ src/Components/Root/Banner/Banner.jsx
@@ -1,9 +1,28 @@
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/bundle";
+import { Navigation, Autoplay, Pagination } from 'swiper/modules';
+import "./Banner.css"
 const Banner = () => {
     return (
         <div>
-            <h3>This is Banner</h3>
-        </div>
+        <Swiper navigation={true} loop={true} modules={[Navigation, Autoplay,Pagination]} pagination={{clickable:true}} autoplay={{delay:2000}} className="mySwiper">
+    <SwiperSlide>
+    <div className="slide slide1 rounded-2xl">
+  
+  </div>
+    </SwiperSlide>
+    <SwiperSlide>
+    <div className="slide slide2 rounded-2xl">
+    
+  </div>
+    </SwiperSlide>
+    <SwiperSlide>
+    <div className="slide slide3 rounded-2xl">
+    
+  </div>
+    </SwiperSlide>
+    </Swiper>
+    </div>
     );
 };
 
